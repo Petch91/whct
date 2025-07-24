ff6b4cd9b8915dc5e903452781d62facdf4358da
Petch91 <pirnay.pierre@gmail.com>
2025-07-24 22:18:18 +0200

    new format code

---
+++ wtct/Program.cs
@@ -0,0 +1,13 @@
+﻿int jour = DateTime.Today.Day;
+int month = DateTime.Today.Month;
+int sum = month + jour;
+int result = sum * 100;
+
+result -= sum;
+result += 3;
+
+Console.WriteLine("UNI" + result);
+
+Console.WriteLine();
+Console.WriteLine("Appuyez sur une touche pour quitter");
+Console.ReadKey();
